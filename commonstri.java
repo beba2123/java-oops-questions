215d2f68ad23d1bf0dd7535f1409a19d34b22ab9
beba2123 <antenehbeba@gmail.com>
2023-04-25 13:58:35 +0300

    common integer elemnts between two arrays

--- commonstri.java
+++ commonstri.java
@@ -1,14 +1,17 @@
+import java.util.HashSet;
+
 public class commonstri {
     public static void main(String[] args) {
         String [] a = {"beba", "Anteneh", "Alem", "miki"};
-        String [] b = {"beba", "eyu", "alemss"};
-
+        String [] b = {"beba", "eyu", "alemss", "miki"};
+        HashSet<String> set = new HashSet<String>();
         for(int i =0;i<a.length;i++){
             for(int j =0;j<b.length; j++){
                  if(a[i].equals(b[j])){
-                    System.out.println(" common elements between two arrays ==> " + a[i]);
+                    set.add(a[i]);
                  }
             }
         }
+        System.out.println(" common elements between two arrays ==> " + set);
     }
 }
