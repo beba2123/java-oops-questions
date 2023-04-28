3f9c72c2e228f16129e609b458cac27c9c7c40b6
beba2123 <antenehbeba@gmail.com>
2023-04-28 11:24:16 +0300

    binary search recusrive

---
+++ recurBinary.java
@@ -0,0 +1,25 @@
+public class recurBinary{
+    public static void main(String[] args) {
+      int a[] ={1,2,4,6,8,9,12,34,55};
+      System.out.println( " the index of the element is " + BSC(a,0,9,55));  
+    }
+    private static int BSC(int a[], int start, int end, int value){
+        
+        if(start>end){     //the base condition for the method..
+            return -1;
+        }
+        int middle = (start+end)/2;
+        int  middlenum = a[middle];
+        if(value==middlenum){
+            return middle;
+        } 
+        if(value> middlenum){
+            return  BSC(a, middle+1, end, value);
+        }
+        else{
+            return BSC(a, start, middle-1, value);
+        }
+        
+       
+    }
+}
